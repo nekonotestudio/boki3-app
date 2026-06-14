8ad69aada15c80c33b5a70c2f347ae39b3b2ccdf
nekonotestudio <fusako.obana@gmail.com>
2026-06-14 10:33:59 +0900

    簿記3級一問一答アプリ 全100問（仕訳中心・4分野）

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'boki3-v1';
+const ASSETS = [
+  '/boki3-app/',
+  '/boki3-app/index.html',
+  '/boki3-app/questions.js',
+  '/boki3-app/questions2.js',
+  '/boki3-app/manifest.json',
+  '/boki3-app/icon-192.png',
+  '/boki3-app/icon-512.png'
+];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request))); });
